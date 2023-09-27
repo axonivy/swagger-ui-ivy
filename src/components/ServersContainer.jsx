767a38d5f08a12198ccc15e9407229fe32d2710e
Lukas Lieb <lukas.lieb@axonivy.com>
2023-09-27 07:42:26 +0200

    wip

--- src/components/ServersContainer.jsx
+++ src/components/ServersContainer.jsx
@@ -1,29 +1,27 @@
 //copied from: swagger-api/swagger-ui/src/core/plugins/oas3/components/servers-container.jsx
-import React from "react"
-import PropTypes from "prop-types"
+import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class ServersContainer extends React.Component {
-
   static propTypes = {
     specSelectors: PropTypes.object.isRequired,
     oas3Selectors: PropTypes.object.isRequired,
     oas3Actions: PropTypes.object.isRequired,
-    getComponent: PropTypes.func.isRequired,
-  }
+    getComponent: PropTypes.func.isRequired
+  };
 
-  render () {
-    const {specSelectors, oas3Selectors} = this.props
+  render() {
+    const { specSelectors, oas3Selectors } = this.props;
 
-    const servers = specSelectors.servers()
+    const servers = specSelectors.servers();
 
     return servers && servers.size ? (
       <div>
-        <div className={"computed-url"}>
+        <div className='computed-url'>
           Server Url:
-          <code>
-            {oas3Selectors.serverEffectiveValue(servers.first()?.get("url"))}
-          </code>
+          <code> {oas3Selectors.serverEffectiveValue(servers.first()?.get('url'))}</code>
         </div>
-      </div> ) : null
+      </div>
+    ) : null;
   }
 }

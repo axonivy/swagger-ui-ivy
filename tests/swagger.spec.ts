6b56c99ec7a20d245da547ab4232caf77d4f193f
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2025-01-20 14:15:55 +0100

    XIVY-15847 reproduce: verify response is parsed

--- tests/swagger.spec.ts
+++ tests/swagger.spec.ts
@@ -5,4 +5,10 @@ test('swagger ui renderer', async ({ page }) => {
 
   await expect(page).toHaveTitle(/API Browser/);
   await expect(page.getByText('Authorize')).toBeVisible();
+
+  // assert response is parsed
+  await expect(page.locator("#operations-demo-getPersons")).toBeVisible();
+  await page.locator("#operations-demo-getPersons").click();
+  await expect(page.locator("#operations-demo-getPersons div.highlight-code"))
+  .toHaveText(/.*"firstname"/);
 });
